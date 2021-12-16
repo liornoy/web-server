5e2752626bd8f7b8872a01cdd6312f768794771e
Lior Noy <ironoy@gmail.com>
2021-12-16 22:41:40 +0200

    Add project skeleton
    Added webserver class supporting of establish multiple connections
    with TCP.
    TODO: add the HTTP logic and divide to sub-classes and functions

---
+++ web-server/WebServer.cpp
@@ -0,0 +1,234 @@
+#include "WebServer.h"
+
+void main()
+{
+	WebServer server;
+	server.Run();
+}
+void WebServer::Run(){
+
+	// Init Server's Listen Socket
+	initListenSocket();
+	
+
+
+
+	while (true)
+	{
+		fd_set waitRecv;
+		FD_ZERO(&waitRecv);
+
+		fd_set waitSend;
+		FD_ZERO(&waitSend);
+
+		std::list<SocketState>::iterator it;
+
+		for (it = sockets.begin(); it != sockets.end(); ++it) {
+			if (((*it).recv == LISTEN) || ((*it).recv == RECEIVE))
+				FD_SET((*it).id, &waitRecv);
+			if ((*it).send == SEND_RESPONSE)
+				FD_SET((*it).id, &waitSend);
+		}
+
+		int nfd;
+		nfd = select(0, &waitRecv, &waitSend, NULL, NULL);
+
+		if (nfd == SOCKET_ERROR)
+		{
+			cout << "Server: Error at select(): " << WSAGetLastError() << endl;
+			WSACleanup();
+			return;
+		}
+
+		for (it = sockets.begin(); it != sockets.end() && nfd > 0; ++it) {
+			if (FD_ISSET((*it).id, &waitRecv))
+			{
+				nfd--;
+				switch ((*it).recv)
+				{
+				case LISTEN:
+					acceptConnection((*it).id);
+					break;
+
+				case RECEIVE:
+					if (!receiveMessage(&(*it))) {
+						it = sockets.erase(it);
+						--it;
+					}
+					break;
+				}
+			}
+		}
+		for (it = sockets.begin(); it != sockets.end() && nfd > 0; ++it) {
+			if (FD_ISSET((*it).id, &waitSend))
+			{
+				nfd--;
+				sendMessage(&(*it));
+			}
+		}
+	}
+
+	closeServer();
+}
+
+void WebServer::closeServer() {
+	cout << "Server: Server Shutting Down.\n";
+	closesocket(listenSocket);
+	WSACleanup();
+}
+
+void WebServer::initListenSocket() {
+	WSAData wsaData;
+
+	if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData))
+	{
+		cout << "Server: Error at WSAStartup()\n";
+		return;
+	}
+
+	listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (INVALID_SOCKET == listenSocket)
+	{
+		cout << "Server: Error at socket(): " << WSAGetLastError() << endl;
+		WSACleanup();
+		return;
+	}
+
+	sockaddr_in serverService;
+
+	serverService.sin_addr.s_addr = INADDR_ANY;
+	serverService.sin_family = AF_INET;
+	serverService.sin_port = htons(SERVER_PORT);
+
+	if (SOCKET_ERROR == bind(listenSocket, (SOCKADDR*)&serverService, sizeof(serverService)))
+	{
+		cout << "Server: Error at bind(): " << WSAGetLastError() << endl;
+		closesocket(listenSocket);
+		WSACleanup();
+		return;
+	}
+
+	if (SOCKET_ERROR == listen(listenSocket, 5))
+	{
+		cout << "Server: Error at listen(): " << WSAGetLastError() << endl;
+		closesocket(listenSocket);
+		WSACleanup();
+		return;
+	}
+
+	cout << "Server is ready and running on address " << inet_ntoa(serverService.sin_addr) << ":" << SERVER_PORT << endl;
+
+	addSocket(listenSocket, LISTEN);
+}
+bool WebServer::addSocket(SOCKET id, int what)
+{
+	if (sockets.size() < MAX_SOCKETS) {
+		SocketState socket;
+
+		socket.id = id;
+		socket.recv = what;
+		socket.send = IDLE;
+		socket.len = 0;
+
+		sockets.push_back(socket);
+		return (true);
+	}
+	return (false);
+}
+
+
+void WebServer::acceptConnection(SOCKET id)
+{
+	struct sockaddr_in from;
+	int fromLen = sizeof(from);
+
+	SOCKET msgSocket = accept(id, (struct sockaddr*)&from, &fromLen);
+	if (INVALID_SOCKET == msgSocket)
+	{
+		cout << "Server: Error at accept(): " << WSAGetLastError() << endl;
+		return;
+	}
+	cout << "Server: Client " << inet_ntoa(from.sin_addr) << ":" << ntohs(from.sin_port) << " is connected." << endl;
+
+	//
+	// Set the socket to be in non-blocking mode.
+	//
+	unsigned long flag = 1;
+	if (ioctlsocket(msgSocket, FIONBIO, &flag) != 0)
+	{
+		cout << "Server: Error at ioctlsocket(): " << WSAGetLastError() << endl;
+	}
+
+	if (addSocket(msgSocket, RECEIVE) == false)
+	{
+		cout << "\t\tToo many connections, dropped!\n";
+		closesocket(id);
+	}
+	return;
+}
+
+bool WebServer::receiveMessage(SocketState* socket)
+{
+	SOCKET msgSocket = (*socket).id;
+
+	int len = (*socket).len;
+	int bytesRecv = recv(msgSocket,&(*socket).buffer[len], sizeof((*socket).buffer) - len, 0);
+
+	if (SOCKET_ERROR == bytesRecv)
+	{
+		cout << "Server: Error at recv(): " << WSAGetLastError() << endl;
+
+		closesocket(msgSocket);
+		return false;
+	}
+
+	if (bytesRecv == 0)
+	{
+		closesocket(msgSocket);
+		return false;
+	}
+
+	else
+	{
+		(*socket).buffer[len + bytesRecv] = '\0'; //add the null-terminating to make it a string
+		cout << "Server: Recieved: " << bytesRecv << " bytes of \"" << &(*socket).buffer[len] << "\" message.\n";
+
+		(*socket).len += bytesRecv;
+
+		if ((*socket).len > 0)
+		{
+			(*socket).send = HANDLE_REQ;
+			//Request request = new Request (socket.id, socket.buffer);
+			//socket.buff = 0;
+			//socket.len = 0;
+			//socket 
+		}
+		return true;
+	}
+
+}
+
+void WebServer::sendMessage(SocketState* socket_ptr)
+{
+	int bytesSent = 0;
+	char sendBuff[255];
+
+	SOCKET msgSocket = (*socket_ptr).id;
+	strcpy(sendBuff, "response");
+
+	bytesSent = send(msgSocket, sendBuff, (int)strlen(sendBuff), 0);
+	if (SOCKET_ERROR == bytesSent)
+	{
+		cout << "Server: Error at send(): " << WSAGetLastError() << endl;
+		return;
+	}
+
+	cout << "Server: Sent: " << bytesSent << "\\" << strlen(sendBuff) << " bytes of \"" << sendBuff << "\" message.\n";
+
+	(*socket_ptr).send = IDLE;
+}
+
+bool SocketState::operator==(const SocketState& other) const {
+	return id == other.id;
+}
---
+++ web-server/WebServer.h
@@ -0,0 +1,51 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+using namespace std;
+#pragma comment(lib, "Ws2_32.lib")
+#include <winsock2.h>
+#include <string.h>
+#include <time.h>
+#include <list>
+
+const int SERVER_PORT = 27015;
+const int MAX_SOCKETS = 60;
+const int EMPTY = 0;
+const int LISTEN = 1;
+const int RECEIVE = 2;
+const int IDLE = 3;
+const int HANDLE_REQ = 4;
+const int PROC_REQUEST = 1;
+const int SEND_RESPONSE = 2;
+
+struct SocketState
+{
+	SOCKET id;			// Socket handle
+	int	recv;			// Receiving?
+	int	send;			// Sending?
+	char buffer[128];
+	int len;
+
+public: 
+	bool operator==(const SocketState& other) const;
+};
+
+list<SocketState>sockets;
+
+
+
+class WebServer
+{
+public:
+	void Run();
+private:
+	SOCKET listenSocket;
+	void closeServer();
+	bool addSocket(SOCKET id, int what);
+	void acceptConnection(SOCKET id);
+	bool receiveMessage(SocketState* socket);
+	void sendMessage(SocketState* socket_ptr);
+	void initListenSocket();
+};
+
