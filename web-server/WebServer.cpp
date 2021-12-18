f4015c343bb9d5984897dd365ceedf1c4b4e816e
Lior Noy <ironoy@gmail.com>
2021-12-18 12:31:25 +0200

    Rearrange functions order

--- web-server/WebServer.cpp
+++ web-server/WebServer.cpp
@@ -13,8 +13,93 @@ namespace web_server {
 		this->logger = &Logger::instance();
 	}
 
-	int WebServer::selectSockets(fd_set* waitRecv, fd_set* waitSend) {
+	void WebServer::run() {
+		// Init Server's Listen Socket
+		SOCKET listenSocketId = initListenSocket();
 
+		if (listenSocketId == NULL) {
+			return;
+		}
+
+		fd_set waitRecv, waitSend;
+		int numOfFD;
+
+		while (true)
+		{
+			numOfFD = selectSockets(&waitRecv, &waitSend);
+
+			if (SOCKET_ERROR == numOfFD) {
+				cerr << "Server: Error at select(): " << WSAGetLastError() << endl;
+				WSACleanup();
+				return;
+			}
+			handleWaitRecv(numOfFD, &waitRecv);
+			handleWaitSend(numOfFD, &waitSend);
+		}
+
+
+		logger->log(Info, "Server: Server Shutting Down.");
+		closesocket(listenSocketId);
+		WSACleanup();
+	}
+
+	SOCKET WebServer::initListenSocket() {
+		WSAData wsaData;
+
+		if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData)) {
+			cerr << "Server: Error at WSAStartup()\n";
+			return NULL;
+		}
+
+		SOCKET socketID = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+		if (INVALID_SOCKET == socketID) {
+			cerr << "Server: Error at socket(): " << WSAGetLastError() << endl;
+			WSACleanup();
+			return NULL;
+		}
+
+		sockaddr_in serverService;
+
+		serverService.sin_addr.s_addr = INADDR_ANY;
+		serverService.sin_family = AF_INET;
+		serverService.sin_port = htons(serverPort);
+
+		if (SOCKET_ERROR == bind(socketID, (SOCKADDR*)&serverService, sizeof(serverService))) {
+			cerr << "Server: Error at bind(): " << WSAGetLastError() << endl;
+			closesocket(socketID);
+			WSACleanup();
+			return NULL;
+		}
+
+		if (SOCKET_ERROR == listen(socketID, 5)) {
+			cerr << "Server: Error at listen(): " << WSAGetLastError() << endl;
+			closesocket(socketID);
+			WSACleanup();
+			return NULL;
+		}
+
+		ss << "Server is ready and running on address " << inet_ntoa(serverService.sin_addr) << ":" << SERVER_PORT;
+		logger->log(Info, ss.str());
+		ss.str("");
+		ss.clear();
+
+		addSocket(socketID, LISTEN);
+
+		return socketID;
+	}
+
+	bool WebServer::addSocket(SOCKET id, int recvStatus)
+	{
+		if (sockets.size() < maxSockets) {
+			Socket newSocket(id, recvStatus);
+			sockets.push_back(newSocket);
+			return true;
+		}
+		return false;
+	}
+
+	int WebServer::selectSockets(fd_set* waitRecv, fd_set* waitSend) {
 		FD_ZERO(waitRecv);
 		FD_ZERO(waitSend);
 
@@ -53,6 +138,7 @@ namespace web_server {
 			}
 		}
 	}
+
 	void WebServer::handleWaitSend(int& numOfFD, fd_set* waitSend) {
 		list<Socket>::iterator it;
 		for (it = sockets.begin(); it != sockets.end() && numOfFD > 0; ++it) {
@@ -62,106 +148,6 @@ namespace web_server {
 				sendMessage(&(*it));
 			}
 		}
-	}
-	void WebServer::run() {
-		// Init Server's Listen Socket
-		SOCKET listenSocketId = initListenSocket();
-
-		if (listenSocketId == NULL) {
-			return;
-		}
-
-		fd_set waitRecv, waitSend;
-		int numOfFD;
-
-		while (true)
-		{
-			numOfFD = selectSockets(&waitRecv, &waitSend);
-
-			if (SOCKET_ERROR == numOfFD) {
-				cerr << "Server: Error at select(): " << WSAGetLastError() << endl;
-				WSACleanup();
-				return;
-			}
-			handleWaitRecv(numOfFD, &waitRecv);
-			handleWaitSend(numOfFD, &waitSend);
-			}
-		
-
-		logger->log(Info,"Server: Server Shutting Down.");
-		closesocket(listenSocketId);
-		WSACleanup();
-	}
-
-
-	SOCKET WebServer::initListenSocket() {
-		WSAData wsaData;
-		
-		if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData)) {
-			cerr << "Server: Error at WSAStartup()\n";
-			return NULL;
-		}
-
-		SOCKET socketID = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-		if (INVALID_SOCKET == socketID) {
-			cerr << "Server: Error at socket(): " << WSAGetLastError() << endl;
-			WSACleanup();
-			return NULL;
-		}
-
-		sockaddr_in serverService;
-
-		serverService.sin_addr.s_addr = INADDR_ANY;
-		serverService.sin_family = AF_INET;
-		serverService.sin_port = htons(SERVER_PORT);
-
-		if (SOCKET_ERROR == bind(socketID, (SOCKADDR*)&serverService, sizeof(serverService))) {
-			cerr << "Server: Error at bind(): " << WSAGetLastError() << endl;
-			closesocket(socketID);
-			WSACleanup();
-			return NULL;
-		}
-
-		if (SOCKET_ERROR == listen(socketID, 5)) {
-			cerr << "Server: Error at listen(): " << WSAGetLastError() << endl;
-			closesocket(socketID);
-			WSACleanup();
-			return NULL;
-		}
-
-		ss << "Server is ready and running on address " << inet_ntoa(serverService.sin_addr) << ":" << SERVER_PORT;
-		logger->log(Info, ss.str());
-		ss.str("");
-		ss.clear();
-
-		addSocket(socketID, LISTEN);
-
-		return socketID;
-	}
-
-	void WebServer::printDisconnectSocket(const SOCKET& socket) {
-		struct sockaddr_in name;
-		int nameLen = sizeof(name);
-
-		if (getpeername(socket, (struct sockaddr*)&name, &nameLen) == SOCKET_ERROR) {
-			cerr << "Server: Error at getpeername(): " << WSAGetLastError() << endl;
-		}
-
-		ss << "Server: Client " << inet_ntoa(name.sin_addr) << ":" << ntohs(name.sin_port) << " has disconnected.";
-		logger->log(Info, ss.str());
-		ss.str("");
-		ss.clear();
-	}
-
-	bool WebServer::addSocket(SOCKET id, int recvStatus)
-	{
-		if (sockets.size() < MAX_SOCKETS) {
-			Socket newSocket(id, recvStatus);
-			sockets.push_back(newSocket);
-			return true;
-		}
-		return false;
 	}
 
 	void WebServer::acceptConnection(Socket& socket)
@@ -264,5 +250,19 @@ namespace web_server {
 		ss.clear();
 
 		(*sokcet).setSocketSendState(IDLE);
+	}
+
+	void WebServer::printDisconnectSocket(const SOCKET& socket) {
+		struct sockaddr_in name;
+		int nameLen = sizeof(name);
+
+		if (getpeername(socket, (struct sockaddr*)&name, &nameLen) == SOCKET_ERROR) {
+			cerr << "Server: Error at getpeername(): " << WSAGetLastError() << endl;
+		}
+
+		ss << "Server: Client " << inet_ntoa(name.sin_addr) << ":" << ntohs(name.sin_port) << " has disconnected.";
+		logger->log(Info, ss.str());
+		ss.str("");
+		ss.clear();
 	}
 }
